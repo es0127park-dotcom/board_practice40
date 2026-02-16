be5cf4035ae90ea2ab3a423c72f172b9717f43cd
parkcoding <es0127.park@gmail.com>
2026-02-16 15:29:16 +0900

    개발시작

---
+++ src/test/java/com/example/board_practice40/BoardPractice40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_practice40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPractice40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
